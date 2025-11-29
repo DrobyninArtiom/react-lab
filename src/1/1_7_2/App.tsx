baa17dcd1f443042f2cebfab50e1c67391f936bf
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-29 22:46:40 +0500

    1_7_2 Created nested lists for recipes and their ingredients using map functions

--- src/1/1_7_2/App.tsx
+++ src/1/1_7_2/App.tsx
@@ -1,7 +1,4 @@
-// 1_7_2 Nested lists in one component 
-/*
-  Создайте список рецептов из этого массива! Для каждого рецепта в массиве выведите его название в виде <h2> и список ингредиентов в виде <ul>. Это потребует вложения двух различных вызовов map.
-*/
+// 1_7_2 Nested lists in one component - Created nested lists for recipes and their ingredients using map functions
 
 import { recipes } from './data';
 
@@ -9,6 +6,16 @@ export default function RecipeList() {
     return (
         <div>
             <h1>Recipes</h1>
+            {recipes.map((recipe) => (
+                <div key={recipe.id}>
+                    <h2>{recipe.name}</h2>
+                    <ul>
+                        {recipe.ingredients.map((ingredient, index) => (
+                            <li key={index}>{ingredient}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
         </div>
     );
 }
