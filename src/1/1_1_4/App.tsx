142667d038946f6ad0c4c39f109fee8f06264990
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-29 19:42:00 +0500

    1_1_4 Created new Congratulations component with success message

--- src/1/1_1_4/App.tsx
+++ src/1/1_1_4/App.tsx
@@ -1,4 +1,8 @@
-// 1_1_4 Your own component 
-/* 
-Напишите компонент с нуля. Вы можете дать ему любое допустимое имя и вернуть любую разметку. Если у вас нет идей, вы можете написать компонент Congratulations, который показывает <h1>Хорошая работа!</h1>. Не забудьте экспортировать это!
-*/
+// 1_1_4 Your own component - Created a new component named Congratulations that displays a success message
+function Congratulations() {
+  return (
+    <h1>Хорошая работа!</h1>
+  );
+}
+
+export default Congratulations;
