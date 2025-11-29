56a83ecf5b7da949c9eb4c761601163810838bda
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-29 23:19:34 +0500

    1_7_4 Added hr separators between paragraphs but not at the beginning or end

--- src/1/1_7_4/App.tsx
+++ src/1/1_7_4/App.tsx
@@ -1,19 +1,4 @@
-// 1_7_4 List with a separator
-/*
-Этот пример отображает знаменитое хайку Кацусики Хокусая, каждая строка которого обернута в тег <p>. Ваша задача — вставить разделитель <hr /> между каждым абзацем. Ваша результирующая структура должна выглядеть следующим образом:
-
-<article>
-    <p>I write, erase, rewrite</p>
-    <hr />
-    <p>Erase again, and then</p>
-    <hr />
-    <p>A poppy blooms.</p>
-</article>
-
-  В хайку всего три строки, но ваше решение должно работать с любым количеством строк. Обратите внимание, что элементы <hr /> появляются только между элементами <p>, а не в начале или конце!
-
-  Это редкий случай, когда индекс в качестве ключа допустим, потому что строки стихотворения никогда не будут перестраиваться.
-*/
+// 1_7_4 List with a separator - Added hr separators between paragraphs but not at the beginning or end
 
 const poem = {
   lines: [
@@ -27,7 +12,10 @@ export default function Poem() {
   return (
       <article>
           {poem.lines.map((line, index) => (
-              <p key={index}>{line}</p>
+              <div key={index}>
+                  <p>{line}</p>
+                  {index < poem.lines.length - 1 && <hr />}
+              </div>
           ))}
       </article>
   );
