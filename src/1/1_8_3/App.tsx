de9d240a4add35cd890be8992847e636382b302a
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-30 01:42:23 +0500

    1_8_3 Fixed by creating a new array instead of mutating the prop array

--- src/1/1_8_3/App.tsx
+++ src/1/1_8_3/App.tsx
@@ -1,9 +1,4 @@
-// 1_8_3 Fix a broken story tray
-/*
-  Генеральный директор вашей компании просит вас добавить "истории" в ваше приложение онлайн-часов, и вы не можете отказать. Вы написали компонент StoryTray, который принимает список stories, за которым следует заполнитель "Create Story".
-
-  Вы реализовали заполнитель "Create Story", поместив еще одну фальшивую историю в конец массива stories, который вы получаете в качестве пропса. Но по какой-то причине "Create Story" появляется более одного раза. Исправьте эту проблему.
-*/
+// 1_8_3 Fix a broken story tray - Fixed by creating a new array instead of mutating the prop array
 
 export interface Story {
   id: string;
@@ -15,15 +10,15 @@ export default function Wrapper(){
   return <StoryTray stories={stories}/>;
 }
 
- function StoryTray({ stories }: { stories: Story[] }) {
-  stories.push({
+function StoryTray({ stories }: { stories: Story[] }) {
+  const storiesWithCreate = stories.concat({
       id: 'create',
       label: 'Create Story',
   });
 
   return (
       <ul>
-          {stories.map((story) => (
+          {storiesWithCreate.map((story) => (
               <li key={story.id}>{story.label}</li>
           ))}
       </ul>
