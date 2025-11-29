890b67baf8595731e72b07a94d08d31730878008
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-29 20:54:25 +0500

    1_5_1 Extracted Profile component with props to reduce code duplication in Gallery

---
+++ src/1/1_5_1/Profile.tsx
@@ -0,0 +1,39 @@
+// 1_5_1 Extract a component - Created Profile component to reduce code duplication
+import { getImageUrl } from "./util";
+
+interface ProfileProps {
+  name: string;
+  imageId: string;
+  profession: string;
+  awards: string;
+  discovered: string;
+}
+
+export default function Profile({ name, imageId, profession, awards, discovered }: ProfileProps) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b> 
+          {profession}
+        </li>
+        <li>
+          <b>Awards: </b> 
+          {awards}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovered}
+        </li>
+      </ul>
+    </section>
+  );
+}
