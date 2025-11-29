9bcf4672f58a1cf5c00007b0a744c7331719d8f6
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-29 18:43:20 +0500

    1_1_2 Fixed return statement issue by placing JSX on same line as return keyword

--- src/1/1_1_2/App.tsx
+++ src/1/1_1_2/App.tsx
@@ -1,11 +1,8 @@
-// 1_1_2 Fix the return statement 
-/* 
-  Исправьте ошибку в операторе return
-*/
-
+// 1_1_2 Fix the return statement - Fixed implicit return issue by placing JSX on the same line as return statement
 export default function Profile() {
-    return
-      <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />;
+    return (
+      <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />
+    );
   }
   
   
