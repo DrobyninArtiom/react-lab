60f8b6687d3f61f9718da7ef4833bd179c4339cf
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-30 13:22:26 +0500

    2_2_4 Removed state and used prompt value directly since state update is asynchronous

--- src/2/2_2_4/App.tsx
+++ src/2/2_2_4/App.tsx
@@ -1,19 +1,9 @@
-// 2_2_4 Remove unnecessary state
-/*
-  При нажатии на кнопку этот пример должен запросить имя пользователя, а затем вывести приветствие. Вы попытались использовать state для хранения имени, но по какой-то причине всегда выводится "Hello, !".
-
-  Чтобы исправить этот код, удалите ненужную переменную state (мы обсудим почему это не сработало позже).
-
-  Можете ли вы объяснить, почему эта переменная состояния была ненужной?
-*/
-
-import { useState } from 'react';
+// 2_2_4 Remove unnecessary state - Removed state and used prompt value directly since state update is asynchronous
 
 export default function FeedbackForm() {
-    const [name, setName] = useState('');
 
     function handleClick() {
-        setName(prompt('What is your name?')??"");
+        const name = prompt('What is your name?')??"";
         alert(`Hello, ${name}!`);
     }
 
