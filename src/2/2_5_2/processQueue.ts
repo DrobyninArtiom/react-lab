0b7cfff11d6db6d4254e300c22d3800f0b695b58
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-11-30 14:23:13 +0500

    2_5_2 Implemented state queue processing with proper handling of values and updater functions

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -1,9 +1,15 @@
+// 2_5_2 Implement the state queue yourself - Implemented state queue processing with proper handling of values and updater functions
 import { Que } from "./App";
-
 export function getFinalState(baseState: number, queue: Que[]) {
     let finalState = baseState;
 
-    // TODO: do something with the queue...
+    for (const update of queue) {
+        if (typeof update === 'function') {
+            finalState = update(finalState);
+        } else {
+            finalState = update;
+        }
+    }
 
     return finalState;
 }
