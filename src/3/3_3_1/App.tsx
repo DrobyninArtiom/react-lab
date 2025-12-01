4372d7e8d6e438e6d359a08b19838855a7f844f9
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-12-01 17:01:33 +0500

    3_3_1 Moved state to parent component to synchronize both inputs

--- src/3/3_3_1/App.tsx
+++ src/3/3_3_1/App.tsx
@@ -1,35 +1,32 @@
-// 3_3_1 Synced inputs 
-/*
-  Эти два входа являются независимыми. Сделайте их синхронизированными: редактирование одного входа должно обновить другой вход с тем же текстом, и наоборот.
-*/
+// 3_3_1 Synced inputs - Moved state to parent component to synchronize both inputs
 
 import { useState } from 'react';
 
 export default function SyncedInputs() {
-  return (
-    <>
-      <Input label="First input" />
-      <Input label="Second input" />
-    </>
-  );
+    const [text, setText] = useState('');
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+            setText(e.target.value);
+        }
+
+    return (
+        <>
+            <Input label="First input" text={text} onChange={handleChange} />
+            <Input label="Second input" text={text} onChange={handleChange} />
+        </>
+    );
 }
 
-function Input({ label }: { label: string }) {
-  const [text, setText] = useState('');
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
-
-  return (
-    <label>
-      {label}
-      {' '}
-      <input
-        value={text}
-        onChange={handleChange}
-      />
-    </label>
-  );
+function Input({ label, text, onChange }: { label: string, text: string, onChange: (e: any) => void }) {
+    return (
+        <label>
+            {label}
+            {' '}
+            <input
+                value={text}
+                onChange={onChange}
+            />
+        </label>
+    );
 }
 
