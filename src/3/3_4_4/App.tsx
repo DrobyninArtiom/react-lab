013a919a61fcaefb31605374f722a3d08816ff1e
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-12-01 19:48:08 +0500

    3_4_4 Added loading state to clear image during loading

--- src/3/3_4_4/App.tsx
+++ src/3/3_4_4/App.tsx
@@ -1,37 +1,44 @@
-// 3_4_4 Clear an image while it’s loading
-/*
-  Когда вы нажимаете кнопку "Далее", браузер начинает загрузку следующего изображения. Однако, поскольку оно отображается в том же теге img, по умолчанию вы будете видеть предыдущее изображение, пока не загрузится следующее. Это может быть нежелательно, если важно, чтобы текст всегда совпадал с изображением. Измените это так, чтобы при нажатии кнопки "Next" предыдущее изображение сразу же убиралось.
-*/
+// 3_4_4 Clear an image while it's loading - Added loading state to clear image during loading
 
 import { useState } from 'react';
 
 export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const hasNext = index < images.length - 1;
+    const [index, setIndex] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const hasNext = index < images.length - 1;
 
-  function handleClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
+    function handleClick() {
+        setIsLoading(true);
+        if (hasNext) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
     }
-  }
 
-  let image = images[index];
-  return (
-    <>
-      <button onClick={handleClick}>
-        Next
-      </button>
-      <h3>
-        Image {index + 1} of {images.length}
-      </h3>
-      <img src={image.src} />
-      <p>
-        {image.place}
-      </p>
-    </>
-  );
+    let image = images[index];
+    return (
+        <>
+            <button onClick={handleClick}>
+                Next
+            </button>
+            <h3>
+                Image {index + 1} of {images.length}
+            </h3>
+            {isLoading ? (
+                <p><i>Loading...</i></p>
+            ) : (
+                <img
+                    src={image.src}
+                    onLoad={() => setIsLoading(false)}
+                    onError={() => setIsLoading(false)}
+                />
+            )}
+            <p>
+                {image.place}
+            </p>
+        </>
+    );
 }
 
 let images = [{
