866d0660bbaf4050b58429f742e3b5b2a24de798
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-12-02 14:10:11 +0500

    4_1_1 Fixed by using useRef to store timeout ID and clearing it properly with current property

--- src/4/4_1_1/App.tsx
+++ src/4/4_1_1/App.tsx
@@ -1,27 +1,23 @@
-// 4_1_1  Fix a broken chat input
-/*
-  Если ввести сообщение и нажать "Отправить" то перед появлением сообщения "Отправлено!" произойдет трехсекундная задержка. Кнопка "Отменить" должна остановить появление сообщения "Отправлено!". Она делает это, вызывая clearTimeout для идентификатора таймаута, сохраненного во время handleSend. Однако даже после нажатия кнопки "Отменить" сообщение "Отправлено!" все равно появляется. Найдите причину неработоспособности и устраните ее.
-*/
-
-import { useState } from 'react';
-import { getImageUrl } from "./utils"
+import { useState, useRef } from 'react';
 
 export default function Chat() {
   const [text, setText] = useState('');
   const [isSending, setIsSending] = useState(false);
-  let timeoutID = null;
+  const timeoutID = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   function handleSend() {
     setIsSending(true);
-    timeoutID = setTimeout(() => {
-      alert('Отправлено!');
-      setIsSending(false);
-    }, 3000);
+    timeoutID.current = setTimeout(() => {
+          alert('Отправлено!');
+          setIsSending(false);
+        }, 3000);
   }
 
   function handleUndo() {
     setIsSending(false);
-    clearTimeout(timeoutID);
+    if (timeoutID.current) {
+          clearTimeout(timeoutID.current);
+        }
   }
 
   return (
