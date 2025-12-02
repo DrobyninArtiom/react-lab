67891e766546ea4a6c03cbfb1d9e3d069e5356f0
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-12-02 23:20:21 +0500

    4_8_2 Modified useCounter hook to accept delay as a parameter and updated component to pass delay value

--- src/4/4_8_2/useCounter.ts
+++ src/4/4_8_2/useCounter.ts
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 
-export function useCounter() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCount(c => c + 1);
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
-  return count;
+// 4_8_2 Make the counter delay configurable - Modified useCounter hook to accept delay as a parameter
+export function useCounter(delay: number) {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        const id = setInterval(() => {
+            setCount(c => c + 1);
+        }, delay);
+        return () => clearInterval(id);
+    }, [delay]);
+    return count;
 }
