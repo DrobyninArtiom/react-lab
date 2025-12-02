ffc90f2af43603a0996910705c8f64a686d84e4c
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-12-02 21:47:18 +0500

    4_6_1 Fixed by adding increment to dependency array to ensure timer uses current increment value

--- src/4/4_6_1/App.tsx
+++ src/4/4_6_1/App.tsx
@@ -1,25 +1,19 @@
-// 4_6_1 Fix a variable that doesn’t update 
-/*
-  Этот компонент Timer хранит переменную состояния count, которая увеличивается каждую секунду. Значение, на которое она увеличивается, хранится в переменной состояния increment. Вы можете управлять переменной increment с помощью кнопок плюс и минус.
-
-  Однако, сколько бы раз вы ни нажали на кнопку с плюсом, счетчик все равно увеличивается на единицу каждую секунду. Что не так с этим кодом? Почему increment всегда равен 1 в коде Effect'а? Найдите ошибку и исправьте ее.
-*/
+// 4_6_1 Fix a variable that doesn't update - Added increment to dependency array to ensure timer uses current increment value
 
 import { useState, useEffect } from 'react';
 
 export default function Timer() {
   const [count, setCount] = useState(0);
   const [increment, setIncrement] = useState(1);
-
+  
   useEffect(() => {
-    const id = setInterval(() => {
-      setCount(c => c + increment);
-    }, 1000);
-    return () => {
-      clearInterval(id);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+      const id = setInterval(() => {
+          setCount(c => c + increment);
+      }, 1000);
+      return () => {
+          clearInterval(id);
+      };
+  }, [increment]);
 
   return (
     <>
