cf3bc7f06e17af1c339f0e02e0992b6dc95e9ad6
DrobyninArtiom <artiom.drobynin@yandex.ru>
2025-12-02 23:47:53 +0500

    4_8_3 Extracted interval logic into a separate useInterval hook and updated useCounter to use it

--- src/4/4_8_3/useCounter.ts
+++ src/4/4_8_3/useCounter.ts
@@ -1,20 +1,11 @@
-import { useState, useEffect } from 'react';
+// 4_8_3 Extract useInterval out of useCounter - Extracted interval logic into a separate useInterval hook
+import { useState } from 'react';
+import { useInterval } from './useInterval';
 
 export function useCounter(delay: number = 1000) {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCount(c => c + 1);
-    }, delay);
-    return () => clearInterval(id);
-  }, [delay]);
+  useInterval(() => {
+    setCount(c => c + 1);
+  }, delay);
   return count;
 }
-// Новая версия
-// export function useCounter(delay) {
-//     const [count, setCount] = useState(0);
-//     useInterval(() => {
-//         setCount((c) => c + 1);
-//     }, delay);
-//     return count;
-//   }
